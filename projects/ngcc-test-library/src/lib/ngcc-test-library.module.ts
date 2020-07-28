345050871e46bac0887597d2a0874b2c8768ac49
Ian Westerfield <iwesterfield@satcomdirect.com>
2020-07-28 11:27:09 -0400

    Project is now stable using libraries with secondary entry points

--- projects/ngcc-test-library/src/lib/ngcc-test-library.module.ts
+++ projects/ngcc-test-library/src/lib/ngcc-test-library.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { NgccTestLibraryComponent } from './ngcc-test-library.component';
+import { NgccTestSecondaryModule } from 'ngcc-test-library/ngcc-test-secondary';
+
 
 @NgModule({
   declarations: [NgccTestLibraryComponent],
-  imports: [],
+  imports: [NgccTestSecondaryModule],
   exports: [NgccTestLibraryComponent]
 })
 export class NgccTestLibraryModule { }
